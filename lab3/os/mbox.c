6ec47bcbe35d31eae68fc608dd30b67ad629e9d8
Rui Wang <ruiwang1998@outlook.com>
2020-03-19 01:02:06 -0400

    still cannot figure out why a deadlock happens

--- lab3/os/mbox.c
+++ lab3/os/mbox.c
@@ -177,7 +177,7 @@ int MboxSend(mbox_t handle, int length, void* message) {
 	if (!mbox_list[handle].pid[GetCurrentPid()]) {
 		return MBOX_FAIL;
 	}
-	if (mbox_list[handle].total_messages >= MBOX_MAX_BUFFERS_PER_MBOX) {	// wait till the mail box is not full
+	while (mbox_list[handle].total_messages >= MBOX_MAX_BUFFERS_PER_MBOX) {	// wait till the mail box is not full
 		CondHandleWait(&(mbox_list[handle].cond_full));
 	}
 
@@ -231,10 +231,10 @@ int MboxRecv(mbox_t handle, int maxlength, void* message) {
 	if (maxlength > MBOX_MAX_MESSAGE_LENGTH){
 		return MBOX_FAIL;
 	}
-	if (!mbox_list[handle].pid[GetCurrentPid()]) {
+	while (!mbox_list[handle].pid[GetCurrentPid()]) {
 		return MBOX_FAIL;
 	}
-	if (mbox_list[handle].total_messages == 0) {
+	while (mbox_list[handle].total_messages == 0) {
 		CondHandleWait(&(mbox_list[handle].cond_empty));
 	}
 	if (SYNC_FAIL==LockHandleAcquire(mbox_list[handle].lock)){
