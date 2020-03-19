471b1387a3552ae48b3dbf930ed3ec6a214d3fea
Rui Wang <ruiwang1998@outlook.com>
2020-03-19 00:44:48 -0400

    fixed the conditional variable useless problem

--- lab3/os/mbox.c
+++ lab3/os/mbox.c
@@ -178,7 +178,7 @@ int MboxSend(mbox_t handle, int length, void* message) {
 		return MBOX_FAIL;
 	}
 	if (mbox_list[handle].total_messages >= MBOX_MAX_BUFFERS_PER_MBOX) {	// wait till the mail box is not full
-		CondHandleWait(mbox_list[handle].cond_full);
+		CondHandleWait(&(mbox_list[handle].cond_full));
 	}
 
 	if (SYNC_FAIL == LockHandleAcquire(mbox_list[handle].lock)){
@@ -203,7 +203,7 @@ int MboxSend(mbox_t handle, int length, void* message) {
 	if (SYNC_FAIL == LockHandleRelease(mbox_list[handle].lock)){
 		return MBOX_FAIL;
 	}
-	CondHandleSignal(mbox_list[handle].cond_empty);
+	CondHandleSignal(&(mbox_list[handle].cond_empty));
 
  return MBOX_SUCCESS;
 }
@@ -236,7 +236,7 @@ int MboxRecv(mbox_t handle, int maxlength, void* message) {
 		return MBOX_FAIL;
 	}
 	if (mbox_list[handle].total_messages == 0) {
-		CondHandleWait(mbox_list[handle].cond_empty);
+		CondHandleWait(&(mbox_list[handle].cond_empty));
 	}
 	if (SYNC_FAIL==LockHandleAcquire(mbox_list[handle].lock)){
 		return MBOX_FAIL;
@@ -257,7 +257,7 @@ int MboxRecv(mbox_t handle, int maxlength, void* message) {
 	if (SYNC_FAIL == LockHandleRelease(mbox_list[handle].lock)){
 		return MBOX_FAIL;
 	}
-	CondHandleSignal(mbox_list[handle].cond_full);
+	CondHandleSignal(&(mbox_list[handle].cond_full));
   
   return mes->size;
 }
