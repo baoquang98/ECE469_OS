4809d33caf72e9d1110782976d75d0cd126b24e6
Rui Wang <ruiwang1998@outlook.com>
2020-04-14 11:29:11 +0800

    start testing 1 for q2

--- lab4/one-level/apps/example/makeprocs/makeprocs.c
+++ lab4/one-level/apps/example/makeprocs/makeprocs.c
@@ -1,7 +1,12 @@
 #include "usertraps.h"
 #include "misc.h"
 
-#define HELLO_WORLD "hello_world.dlx.obj"
+#define TEST1 "test1.dlx.obj"
+
+void test1(char * s_procs_completed_str){
+  Printf("Start hello world testing: Print 'Hello World' and exit.");
+  process_create(TEST1, s_procs_completed_str, NULL);
+}
 
 void main (int argc, char *argv[])
 {
@@ -10,40 +15,42 @@ void main (int argc, char *argv[])
   sem_t s_procs_completed;             // Semaphore used to wait until all spawned processes have completed
   char s_procs_completed_str[10];      // Used as command-line argument to pass page_mapped handle to new processes
 
-  if (argc != 2) {
-    Printf("Usage: %s <number of hello world processes to create>\n", argv[0]);
+  if (argc != 1) {
+    Printf("Usage: no argument, this just runs the tests\n");
     Exit();
   }
 
   // Convert string from ascii command line argument to integer number
-  num_hello_world = dstrtol(argv[1], NULL, 10); // the "10" means base 10
-  Printf("makeprocs (%d): Creating %d hello_world processes\n", getpid(), num_hello_world);
+  // num_hello_world = dstrtol(argv[1], NULL, 10); // the "10" means base 10
+  // Printf("makeprocs (%d): Creating %d hello_world processes\n", getpid(), num_hello_world);
 
   // Create semaphore to not exit this process until all other processes 
   // have signalled that they are complete.
-  if ((s_procs_completed = sem_create(0)) == SYNC_FAIL) {
-    Printf("makeprocs (%d): Bad sem_create\n", getpid());
-    Exit();
-  }
+  // if ((s_procs_completed = sem_create(0)) == SYNC_FAIL) {
+  //   Printf("makeprocs (%d): Bad sem_create\n", getpid());
+  //   Exit();
+  // }
 
   // Setup the command-line arguments for the new processes.  We're going to
   // pass the handles to the semaphore as strings
   // on the command line, so we must first convert them from ints to strings.
   ditoa(s_procs_completed, s_procs_completed_str);
 
-  // Create Hello World processes
-  Printf("-------------------------------------------------------------------------------------\n");
-  Printf("makeprocs (%d): Creating %d hello world's in a row, but only one runs at a time\n", getpid(), num_hello_world);
-  for(i=0; i<num_hello_world; i++) {
-    Printf("makeprocs (%d): Creating hello world #%d\n", getpid(), i);
-    process_create(HELLO_WORLD, s_procs_completed_str, NULL);
-    if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
-      Printf("Bad semaphore s_procs_completed (%d) in %s\n", s_procs_completed, argv[0]);
-      Exit();
-    }
-  }
+  test1(s_procs_completed_str);
 
-  Printf("-------------------------------------------------------------------------------------\n");
-  Printf("makeprocs (%d): All other processes completed, exiting main process.\n", getpid());
+  // Create Hello World processes
+  // Printf("-------------------------------------------------------------------------------------\n");
+  // Printf("makeprocs (%d): Creating %d hello world's in a row, but only one runs at a time\n", getpid(), num_hello_world);
+  // for(i=0; i<num_hello_world; i++) {
+  //   Printf("makeprocs (%d): Creating hello world #%d\n", getpid(), i);
+  //   process_create(HELLO_WORLD, s_procs_completed_str, NULL);
+  //   if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
+  //     Printf("Bad semaphore s_procs_completed (%d) in %s\n", s_procs_completed, argv[0]);
+  //     Exit();
+  //   }
+  // }
+
+  // Printf("-------------------------------------------------------------------------------------\n");
+  // Printf("makeprocs (%d): All other processes completed, exiting main process.\n", getpid());
 
 }
