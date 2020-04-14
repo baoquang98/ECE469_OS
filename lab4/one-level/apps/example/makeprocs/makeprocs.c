53a6763f4d186eb7dee622355f8b1d953ef745f0
Rui Wang <ruiwang1998@outlook.com>
2020-04-14 13:10:58 +0800

    wait for each test to be over s.t. succession

--- lab4/one-level/apps/example/makeprocs/makeprocs.c
+++ lab4/one-level/apps/example/makeprocs/makeprocs.c
@@ -11,10 +11,10 @@
 
 void test_wrapper(int test_id){
 
-  sem_t s_procs_completed;             // Semaphore used to wait until all spawned processes have completed
+  sem_t test_complete;             // Semaphore used to wait until all spawned processes have completed
   char s_procs_completed_str[10];      // Used as command-line argument to pass page_mapped handle to new processes
 
-  if ((s_procs_completed = sem_create(0)) == SYNC_FAIL) {
+  if ((test_complete = sem_create(0)) == SYNC_FAIL) {
     Printf("makeprocs (%d): Bad sem_create\n", getpid());
     Exit();
   }
@@ -22,7 +22,7 @@ void test_wrapper(int test_id){
   // Setup the command-line arguments for the new processes.  We're going to
   // pass the handles to the semaphore as strings
   // on the command line, so we must first convert them from ints to strings.
-  ditoa(s_procs_completed, s_procs_completed_str);
+  ditoa(test_complete, s_procs_completed_str);
 
   switch(test_id) {
     case 1: 
@@ -31,14 +31,22 @@ void test_wrapper(int test_id){
     case 2:
       process_create(TEST2, s_procs_completed_str, NULL);
       // Signal the semaphore to tell the original process that we're done
-      if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
-        Printf("TEST2 (%d): Bad semaphore s_procs_completed (%d)!\n", getpid(), s_procs_completed);
+      if(sem_signal(test_complete) != SYNC_SUCCESS) {
+        Printf("TEST2 (%d): Bad semaphore test_complete (%d)!\n", getpid(), test_complete);
         Exit();
       }
       break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
   }
-  if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
-    Printf("Bad semaphore s_procs_completed (%d) in TEST%d\n", s_procs_completed, test_id);
+  if (sem_wait(test_complete) != SYNC_SUCCESS) {
+    Printf("Bad semaphore test_complete (%d) in TEST%d\n", test_complete, test_id);
     Exit();
   }
 }
@@ -68,14 +76,16 @@ void main (int argc, char *argv[])
   test_wrapper(1);
   Printf("-------------------------------------------------------------------------------------\n");
   test_wrapper(2);
+  Printf("-------------------------------------------------------------------------------------\n");
+  test_wrapper(3);
   // Create Hello World processes
   // Printf("-------------------------------------------------------------------------------------\n");
   // Printf("makeprocs (%d): Creating %d hello world's in a row, but only one runs at a time\n", getpid(), num_hello_world);
   // for(i=0; i<num_hello_world; i++) {
   //   Printf("makeprocs (%d): Creating hello world #%d\n", getpid(), i);
   //   process_create(HELLO_WORLD, s_procs_completed_str, NULL);
-  //   if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
-  //     Printf("Bad semaphore s_procs_completed (%d) in %s\n", s_procs_completed, argv[0]);
+  //   if (sem_wait(test_complete) != SYNC_SUCCESS) {
+  //     Printf("Bad semaphore test_complete (%d) in %s\n", test_complete, argv[0]);
   //     Exit();
   //   }
   // }
