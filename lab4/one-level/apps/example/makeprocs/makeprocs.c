2284a5596cb1866f8b6d1aab28989bdabdab42ec
Rui Wang <ruiwang1998@outlook.com>
2020-04-14 11:38:59 +0800

    take care of semaphores

--- lab4/one-level/apps/example/makeprocs/makeprocs.c
+++ lab4/one-level/apps/example/makeprocs/makeprocs.c
@@ -1,11 +1,23 @@
 #include "usertraps.h"
 #include "misc.h"
 
+// define the user program string
 #define TEST1 "test1.dlx.obj"
+#define TEST2 "test2.dlx.obj"
+#define TEST3 "test3.dlx.obj"
+#define TEST4 "test4.dlx.obj"
+#define TEST5 "test5.dlx.obj"
+#define TEST6 "test6.dlx.obj"
 
-void test1(char * s_procs_completed_str){
-  Printf("Start hello world testing: Print 'Hello World' and exit.");
+void test1(sem_t s_procs_completed, char * s_procs_completed_str){
+  Printf("Start hello world testing: Print 'Hello World' and exit.\n");
   process_create(TEST1, s_procs_completed_str, NULL);
+  Printf("Done with TEST1 in Q2\n");
+
+  if (sem_wait(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("Bad semaphore s_procs_completed (%d) in TEST1\n", s_procs_completed);
+    Exit();
+  }
 }
 
 void main (int argc, char *argv[])
@@ -26,17 +38,17 @@ void main (int argc, char *argv[])
 
   // Create semaphore to not exit this process until all other processes 
   // have signalled that they are complete.
-  // if ((s_procs_completed = sem_create(0)) == SYNC_FAIL) {
-  //   Printf("makeprocs (%d): Bad sem_create\n", getpid());
-  //   Exit();
-  // }
+  if ((s_procs_completed = sem_create(0)) == SYNC_FAIL) {
+    Printf("makeprocs (%d): Bad sem_create\n", getpid());
+    Exit();
+  }
 
   // Setup the command-line arguments for the new processes.  We're going to
   // pass the handles to the semaphore as strings
   // on the command line, so we must first convert them from ints to strings.
   ditoa(s_procs_completed, s_procs_completed_str);
 
-  test1(s_procs_completed_str);
+  test1(s_procs_completed, s_procs_completed_str);
 
   // Create Hello World processes
   // Printf("-------------------------------------------------------------------------------------\n");
