89032362ae526e73c8a20e20b195871294866ac2
Rui Wang <ruiwang1998@outlook.com>
2020-04-14 20:15:02 +0800

    wrong print out

--- lab4/fork/apps/example/fork_test/fork_test.c
+++ lab4/fork/apps/example/fork_test/fork_test.c
@@ -8,18 +8,18 @@ void main (int argc, char *argv[])
 
 	//Print pid from original process
 	Printf("Fork Testing (PID: %d): This is the original process\n", getpid());
-	Printf("Fork Testing (PID: %d): this process holds a variable j of value %d\n", b_Juliana);
+	Printf("Fork Testing (PID: %d): this process holds a variable j of value %d.\n", getpid(), b_Juliana);
 	pid = fork();
 
 	if(pid == 0) {
 		//child process
-		Printf("Fork Testing (PID: %d): This is the child process\n", getpid());
+		Printf("Fork Testing (PID: %d): This is the child process.\n", getpid());
 		Printf("Fork Testing (PID: %d): j was %d.\n", getpid(), b_Juliana);
 		b_Juliana = 1998;
 		Printf("Fork Testing (PID: %d): j now is %d.\n", getpid(), b_Juliana);
 	} else {
 		//parent 
-		Printf("Fork Testing (PID: %d): Hello i'm the respectable parent. My id is %d :)\n", getpid());
+		Printf("Fork Testing (PID: %d): This is the parent process.)\n", getpid());
 		Printf("Fork Testing (PID: %d): j was %d\n", getpid(), b_Juliana);
 		b_Juliana = 902;
 		Printf("Fork Testing (PID: %d): j now is %d\n", getpid(), b_Juliana);
